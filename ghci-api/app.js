c60e5248354c33f0a1ee2b118ba374e8621fe970
yamadapc <tacla.yamada@gmail.com>
2016-01-25 05:00:49 -0200

    Initial commit

---
+++ ghci-api/app.js
@@ -0,0 +1,59 @@
+var browserify = require('browserify-middleware');
+var child_process = require('child_process');
+var express = require('express');
+var byline = require('byline');
+var http = require('http');
+var socketio = require('socket.io');
+
+var app = express();
+
+app.use('/js', browserify(__dirname + '/client'));
+app.use('/static', express.static('./'));
+app.get('/', function(req, res) {
+  res.sendfile('index.html');
+});
+
+var server = http.Server(app);
+
+var io = socketio(server);
+io.on('connection', function(socket) {
+  console.log('User connected; spawning GHCi instance');
+
+  var ghci = child_process.spawn('ghci');
+  ghci.stdin.write(':set prompt >\n');
+  var ghciStdoutByLine = byline(ghci.stdout);
+  var ghciStderrByLine = byline(ghci.stderr);
+  var nextEvent = 'echo';
+
+  function handleGhciData(name, data) {
+    data = data.toString();
+    if(data === '>') return;
+    data = data.replace(/^(> ?)+/g, '');
+    console.log('Writting ' + name, data, 'to socket');
+    socket.emit(nextEvent, data);
+  }
+
+  ghciStdoutByLine.on('data', handleGhciData.bind(null, 'stdout'));
+  ghciStderrByLine.on('data', handleGhciData.bind(null, 'stderr'));
+
+  socket.on('command', function(d) {
+    console.log('Writting', d, 'to GHCi');
+    nextEvent = 'echo';
+    ghci.stdin.write(d + '\n');
+  });
+
+  socket.on('sprint', function(d) {
+    console.log('Handling internal sprint call', d);
+    nextEvent = 'sprint';
+    ghci.stdin.write(':sprint ' + d + '\n');
+  });
+
+  socket.on('disconnect', function() {
+    console.log('User disconnected, killing GHCi');
+    ghci.kill();
+  });
+});
+
+server.listen(3000, function() {
+  console.log('listening on *:3000');
+});
---
+++ ghci-api/client/index.js
@@ -0,0 +1,72 @@
+var $ = require('jquery');
+var Promise = require('bluebird');
+window.jQuery = $;
+require('jquery.terminal');
+
+var socket = io();
+var variablesHash = {};
+
+$('.clear-variables').on('click', function() {
+  variablesHash = {};
+  syncVariables();
+  return false;
+});
+
+$('.activate-form-annotations').on('change', function() {
+  $('.sprint-drawer').toggleClass('annotations', $(this).prop('checked'));
+});
+
+function syncVariable(name) {
+  return new Promise(function(fulfill) {
+    console.log(':sprint ' + name);
+    socket.emit('sprint', name);
+    socket.once('sprint', function(result) {
+      fulfill(result);
+    });
+  });
+}
+
+function syncVariables() {
+  var variables = Object.keys(variablesHash);
+  console.log('Syncing variables', variables);
+  return Promise.mapSeries(variables, function(variable) {
+    return syncVariable(variable);
+  }).then(function(results) {
+    console.log('results =', results);
+    $('.sprint-drawer ul').html(
+      results.map(function(result, i) {
+        if(result.indexOf(variables[i]) === -1) {
+          result = variables[i] + ' = ' + result;
+        }
+
+        var form = /= _$/.test(result) ? '' : (result.indexOf('_') === -1 ? 'nf' : 'whnf');
+        var humanForm = '';
+        if(form === 'nf') humanForm = 'Normal Form';
+        else if(form === 'whnf') humanForm = 'Weak Head Normal Form'
+
+        return '<li class="' + form + '"><code>:sprint ' + variables[i] + ' ===> ' + result + '</code><span class="form">' + humanForm + '</span></li>';
+      }).join('\n')
+    );
+  });
+}
+
+var term = $('.terminal').terminal(function(command) {
+  if(command.indexOf('let ') === 0) {
+    variablesHash[command.slice(4).split(' ')[0]] = true;
+  }
+
+  setTimeout(function() {
+    syncVariables();
+  }, 500);
+
+  socket.emit('command', command);
+}, {
+  greetings: 'Web GHCi - Alpha Version',
+  name: 'web_ghci',
+  height: '100%',
+  prompt: 'ghci> ',
+});
+
+socket.on('echo', function(data) {
+  term.echo(data);
+});
