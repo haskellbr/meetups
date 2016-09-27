4c7de39bb782b1389931b13c7761a6e5417af9f1
yamadapc <tacla.yamada@gmail.com>
2016-09-27 20:23:22 -0300

    Add dataloader example

---
+++ 09-2016/programas-como-linguagens/javascript-dataloader-example/index.js
@@ -0,0 +1,28 @@
+const DataLoader = require('dataloader');
+const Promise = require('bluebird');
+const fetch = require('isomorphic-fetch');
+
+function loggedFetch(url, options) {
+  console.log('fetching', url, '...');
+  return fetch(url, options);
+}
+
+const loader = new DataLoader((urls) => {
+  const seenUrls = {};
+  return Promise.map(urls, (url) => {
+    return loggedFetch(url).then(res => res.text());
+  });
+});
+
+const p1 = loader.load('https://google.com');
+const p2 = loader.load('https://google.com');
+const p3 = loader.load('https://google.com');
+const p4 = loader.load('https://beijaflor.io');
+
+Promise.join(p1, p2, p3, p4).spread((r1, r2, r3, r4) => {
+  console.log('  r1 ->', r1.length);
+  console.log('  r2 ->', r2.length);
+  console.log('  r3 ->', r3.length);
+  console.log('  r4 ->', r4.length);
+  setTimeout(() => {}, 100);
+});
